c8321de5677ee0eeeb4d2f24add9788508f99996
Kevin DeMarco <kevin.demarco@gtri.gatech.edu>
2013-05-28 13:03:18 -0400

    full clean

--- clean.sh
+++ clean.sh
@@ -3,3 +3,9 @@
 echo "Cleaning pycaches..."
 
 find . -name '*pycache*' | xargs rm -rf
+
+echo "Cleaning blender files..."
+find -regex '.*blend[1234567890]' | xargs rm
+
+# Removes files from git repo that have been deleted
+#git rm $(git ls-files --deleted)  
